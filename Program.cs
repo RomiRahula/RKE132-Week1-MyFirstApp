e5a3933b0add3b1660e578f5b562c34cc982553e
Romi <romi.rahula@tktk.ee>
2023-02-11 14:35:44 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// This is a comment to my code
+
+
+Console.WriteLine("Enter your name:");
+string userName = Console.ReadLine();
+Console.WriteLine("Hello" + ", " + userName + "!");
+Console.WriteLine($"Hello, {userName} !");
